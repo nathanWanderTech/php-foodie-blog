8adccf66e8f7cc7eae7af19f229e4b5a7d5853ec
Nathan Le <nha.le@tekos.net>
2023-04-10 13:12:44 -0400

    Add more mock data

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,7 @@ class CategoryFactory extends Factory
     public function definition()
     {
         $defaultPhotos = ['cp-1.jpg', 'cp-1.jpg', 'cp-3.jpg', 'cp-4.jpg', 'cp-5.jpg', 'cp-6.jpg', 'cp-7.jpg', 'cp-8.jpg'];
-        $names = ['Italian', 'Indian', 'Mexican', 'Continental', 'Thai', 'Chinese', 'Mughlai', 'Japanese', 'Vietnamese', 'Vegan', 'Greek'];
+        $names = ['Italian cuisine', 'Indian cuisine', 'Mexican cuisine', 'Continental cuisine', 'Thai cuisine', 'Chinese cuisine', 'Mughlai cuisine', 'Japanese cuisine', 'Vietnamese cuisine', 'Vegan cuisine', 'Greek cuisine', 'Malian cuisine', 'Native American cuisine', 'Latin American cuisine', 'Canadian cuisine', 'Cuisine of New England', 'Southern American cuisine', 'French Guianan cuisine'];
         return [
             //
             'name' => fake()->randomElement($names),
--- resources/views/partials/post/comments.blade.php
+++ resources/views/partials/post/comments.blade.php
@@ -30,4 +30,3 @@
     <hr>
     @endforeach
 </div>
-
