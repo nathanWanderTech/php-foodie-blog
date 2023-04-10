f9fe8f483fddf197565e6ea7bf4f4aca72da8262
Nathan Le <nha.le@tekos.net>
2023-04-09 22:51:07 -0400

    wip on posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index () {
-        return view('posts.index');
+        $posts = Post::paginate(7);
+        return view('posts.index', compact('posts'));
     }
 
     public function show() {
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,42 +14,46 @@
                                     <span>Posts</span>
                                 </div>
                             </div>
-
-                            <div class="categories__list__post__item">
-                                <div class="row">
-                                    <div class="col-lg-6 col-md-6">
-                                        <div class="categories__post__item__pic set-bg"
-                                             data-setbg="{{asset("images/categories/categories-post/cp-1.jpg")}}">
-                                            <div class="post__meta">
-                                                <h4>08</h4>
-                                                <span>Aug</span>
+                                @foreach($posts as $post)
+                                <div class="categories__list__post__item">
+                                    <div class="row">
+                                        <div class="col-lg-6 col-md-6">
+                                            <div class="categories__post__item__pic set-bg"
+                                                 data-setbg="{{asset($post->thumbnail_photo)}}">
+                                                <div class="post__meta">
+                                                    <h4>{{date('d', strtotime($post->updated_at))}}</h4>
+                                                    <span>{{date('M', strtotime($post->updated_at))}}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-md-6">
+                                            <div class="categories__post__item__text">
+                                                <span class="post__label">Dinner</span>
+                                                <h3><a href="#">{{$post->title}}</a>
+                                                </h3>
+                                                <ul class="post__widget">
+                                                    <li>by <span>Admin</span></li>
+                                                    <li>3 min read</li>
+                                                    <li>20 Comment</li>
+                                                </ul>
+                                                <p>{{substr($post->content, 0, 100)}}...</p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-6 col-md-6">
-                                        <div class="categories__post__item__text">
-                                            <span class="post__label">Dinner</span>
-                                            <h3><a href="#">A 5-Minute Peach Mug Cobbler That Just So Happens to...</a>
-                                            </h3>
-                                            <ul class="post__widget">
-                                                <li>by <span>Admin</span></li>
-                                                <li>3 min read</li>
-                                                <li>20 Comment</li>
-                                            </ul>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                                tempor incididunt dolore magna aliqua. Quis ipsum suspendisse ultrices
-                                                gravida...</p>
-                                        </div>
-                                    </div>
                                 </div>
-                            </div>
+                                @endforeach
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="categories__pagination">
-                                        <a href="#">1</a>
-                                        <a href="#">2</a>
-                                        <a href="#">3</a>
-                                        <a href="#">Next</a>
+                                        @if($posts->currentPage() > 1)
+                                            <a href="{{ $posts->url($posts->currentPage() - 1) }}">Back</a>
+                                        @endif
+                                        @for ($i = 1; $i <= $posts->lastPage(); $i++)
+                                            <a href="{{ $posts->url($i) }}">{{ $i }}</a>
+                                        @endfor
+                                        @if($posts->currentPage() < $posts->lastPage())
+                                            <a href="{{ $posts->url($posts->currentPage() + 1) }}">Next</a>
+                                        @endif
                                     </div>
                                 </div>
                             </div>
